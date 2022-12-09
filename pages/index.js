243dd7ee71b0c474e494bb79b12c9b5c7c099689
AndrianarivoAlpha <88578278+AndrianarivoAlpha@users.noreply.github.com>
2022-12-09 08:14:40 +0300

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,56 @@ import React, { useState, useEffect } from 'react'
 import { options, url } from '../lib/options';
 import { timeConverter } from '../lib/utilsFunctions';
 import { InfinitySpin } from 'react-loader-spinner';
-import Graph from '../components/Graph';
 
 const now = new Date();
-const endDate = `${ now.getFullYear() }-${ now.getMonth() + 1 }-${ now.getDate() }`;
-const startDate = `${ now.getFullYear() - 1 }-${ now.getMonth() + 1 }-${ now.getDate() + 1}`;
+const endDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+const startDate = `${now.getFullYear() - 1}-${now.getMonth() + 1}-${now.getDate() + 1}`;
 
-const Home = ( { data, timesSeriesData } ) =>
-{
-  console.log( startDate );
+const Home = ({ data, timesSeriesData }) => {
+  console.log(startDate);
   const { symbols } = data;
 
-  const [ amount, setAmount ] = useState( 1 );
-  const [ from, setFrom ] = useState( "USD" );
-  const [ to, setTo ] = useState( "EUR" );
-  const [ total, setTotal ] = useState( null );
-  const [ date, setDate ] = useState( "" );
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [total, setTotal] = useState(null);
+  const [date, setDate] = useState("");
 
-  const [ isLoading, setIsLoading ] = useState( false )
+  const [isLoading, setIsLoading] = useState(false)
 
-  const getDevise = async () =>
-  {
-    setIsLoading( true )
+  const getDevise = async () => {
+    setIsLoading(true)
 
-    if ( amount > 0 )
-    {
-      const res = await fetch( `${ url }/convert?from=${ from }&to=${ to }&amount=${ amount.toString() }`, options );
+    if (amount > 0) {
+      const res = await fetch(`${url}/convert?from=${from}&to=${to}&amount=${amount.toString()}`, options);
       const data = await res.json();
 
-      if ( data )
-      {
-        setDate( data?.info?.timestamp )
-        setTotal( data?.result )
-        setIsLoading( false )
+      if (data) {
+        setDate(data?.info?.timestamp)
+        setTotal(data?.result)
+        setIsLoading(false)
       }
-    } else
-    {
-      setTotal( 0 )
+    } else {
+      setTotal(0)
     }
 
   }
 
-  useEffect( () =>
-  {
+  useEffect(() => {
     getDevise()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [from, to] )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [from, to])
 
-  const totalTocurrency = new Intl.NumberFormat( 'en-US', { maximumSignificantDigits: 5 } ).format( total );
-  const amountToCurrency = new Intl.NumberFormat( 'en-US', { maximumSignificantDigits: 5 } ).format( amount );
+  const totalTocurrency = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 5 }).format(total);
+  const amountToCurrency = new Intl.NumberFormat('en-US', { maximumSignificantDigits: 5 }).format(amount);
 
   return (
     <div className="flex flex-col items-center gap-2">
       <form
         className="flex items-center m-auto"
-        onSubmit={ ( e ) =>
-        {
+        onSubmit={(e) => {
           e.preventDefault();
-        } }
+        }}
       >
         <div className="relative w-[340px]">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -70,10 +62,9 @@ const Home = ( { data, timesSeriesData } ) =>
             type="text"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
-            value={ amount }
-            onChange={ ( e ) =>
-            {
-              setAmount( e.target.value )
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value)
             }
             }
           />
@@ -83,21 +74,19 @@ const Home = ( { data, timesSeriesData } ) =>
       <select
         id="countries"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[340px]"
-        value={ from }
-        onChange={ ( e ) =>
-        {
-          setFrom( e.target.value );
+        value={from}
+        onChange={(e) => {
+          setFrom(e.target.value);
         }
         }
       >
         {
           symbols &&
-          Object.keys( symbols ).sort().map( ( key, index ) =>
-          {
+          Object.keys(symbols).sort().map((key, index) => {
             return (
-              <option key={ index } value={ key }>{ `${ key } - ${ symbols[ key ] }` }</option>
+              <option key={index} value={key}>{`${key} - ${symbols[key]}`}</option>
             )
-          } )
+          })
         }
       </select>
 
@@ -105,21 +94,19 @@ const Home = ( { data, timesSeriesData } ) =>
       <select
         id="countries"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[340px]"
-        value={ to }
-        onChange={ ( e ) =>
-        {
-          setTo( e.target.value );
+        value={to}
+        onChange={(e) => {
+          setTo(e.target.value);
         }
         }
       >
         {
           symbols &&
-          Object.keys( symbols ).sort().map( ( key, index ) =>
-          {
+          Object.keys(symbols).sort().map((key, index) => {
             return (
-              <option key={ index } value={ key }>{ `${ key } - ${ symbols[ key ] }` }</option>
+              <option key={index} value={key}>{`${key} - ${symbols[key]}`}</option>
             )
-          } )
+          })
         }
       </select>
 
@@ -127,15 +114,15 @@ const Home = ( { data, timesSeriesData } ) =>
         <button
           type="button"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={ () => getDevise() }
+          onClick={() => getDevise()}
         >
           CONVERT
         </button>
       </div>
-      
+
 
       <div className='result-container'>
-        { isLoading ? (
+        {isLoading ? (
           <InfinitySpin
             width='200'
             color="#2797e2"
@@ -146,20 +133,19 @@ const Home = ( { data, timesSeriesData } ) =>
             {
               total && (
                 <div className='text-start mb-5 w-auto bg-gray-50 px-10 py-5 rounded'>
-                  <p className='text-normal font-semibold'>{ amountToCurrency }.00 { from } =</p>
-                  <h1 className='text-3xl font-bold'>{ totalTocurrency } { to }</h1>
+                  <p className='text-normal font-semibold'>{amountToCurrency}.00 {from} =</p>
+                  <h1 className='text-3xl font-bold'>{totalTocurrency} {to}</h1>
                   <br />
                   <p>
-                    1 { from } = { total / amount } { to }
+                    1 {from} = {total / amount} {to}
                   </p>
                   <p>
-                    1 { to } = { amount / total } { from }
+                    1 {to} = {amount / total} {from}
                   </p>
-                  <p className='text-xs font-semibold text-gray-300'>Updated on : { timeConverter( date ) }</p>
+                  <p className='text-xs font-semibold text-gray-300'>Updated on : {timeConverter(date)}</p>
                 </div>
               )
-              }
-              <Graph timesSeriesData = { timesSeriesData } to = { to } from = { from }/>
+            }
           </div>
         )
         }
@@ -171,12 +157,11 @@ const Home = ( { data, timesSeriesData } ) =>
 
 export default Home
 
-export const getServerSideProps = async () =>
-{
-  const res = await fetch( `${ url }/symbols`, options );
+export const getServerSideProps = async () => {
+  const res = await fetch(`${url}/symbols`, options);
   const data = await res.json()
 
-  const timesSeries = await fetch( `${ url }/timeseries?start_date=${ startDate }&end_date=${ endDate }&from=EUR&to=USD`, options );
+  const timesSeries = await fetch(`${url}/timeseries?start_date=${startDate}&end_date=${endDate}&from=EUR&to=USD`, options);
   const timesSeriesData = await timesSeries.json();
 
   return {
